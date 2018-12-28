9355f7c10cbd43add31e623fcca22af95b1e2a33
Dang Tran <thdang1003@gmail.com>
2018-12-28 11:45:55 +0700

    update models

--- backend/app_core/modules/web/users/__init__.py
+++ backend/app_core/modules/web/users/__init__.py
@@ -16,23 +16,23 @@ _logger = logging.getLogger(__name__)
 user = Blueprint('user', __name__)
 
 
-@user.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        data = request.get_json()
-        if 'access_token' in request.cookies:
-            data_token = validate_token(request.cookies['access_token'])
-            return jsonify({'access_token': data_token})
-        elif 'username' in data and 'password' in data:
-            matches_username = re.match(REGEX_USERNAME, data['username'], re.MULTILINE | re.VERBOSE)
-            matches_password = re.match(REGEX_PASSWORD, data['password'], re.MULTILINE | re.VERBOSE)
-            if matches_password != None and matches_username != None:
-                user = User.get_user_by_username_password(data['username'], data['password'])
-                if user:
-                    token = generate_token(user)
-                    UserToken.insert_token(token, user.id)
-                    return jsonify({'token': token})
-                else:
-                    return None
-            else:
-                return jsonify({'noti': "Sai dinh dang"})
+# @user.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         if 'access_token' in request.cookies:
+#             data_token = validate_token(request.cookies['access_token'])
+#             return jsonify({'access_token': data_token})
+#         elif 'username' in data and 'password' in data:
+#             matches_username = re.match(REGEX_USERNAME, data['username'], re.MULTILINE | re.VERBOSE)
+#             matches_password = re.match(REGEX_PASSWORD, data['password'], re.MULTILINE | re.VERBOSE)
+#             if matches_password != None and matches_username != None:
+#                 user = User.get_user_by_username_password(data['username'], data['password'])
+#                 if user:
+#                     token = generate_token(user)
+#                     UserToken.insert_token(token, user.id)
+#                     return jsonify({'token': token})
+#                 else:
+#                     return None
+#             else:
+#                 return jsonify({'noti': "Sai dinh dang"})
