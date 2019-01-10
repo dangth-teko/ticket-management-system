522fbe903faa28ab0d86ba1e57704cb0378edbae
Dang Tran <dang.th@teko.vn>
2019-01-10 10:21:38 +0700

    merge feture/config-nginx

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,9 +1,6 @@
 // export const BASE_URL = 'http://localhost:11111/api'
-<<<<<<< HEAD
 export const BASE_URL = 'https://728c6164.ngrok.io/api'
-=======
-export const BASE_URL = 'http://local-ticket.teko.vn/api'
->>>>>>> origin/feature/config-nginx
+
 
 export const apiConstants = {
     POST_LOGIN: '/signin',
